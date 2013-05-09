2f2192aad94b2d4011030dd1e01dd8f2d3f2441b
lithium <code@hlidskialf.com>
2013-05-08 17:14:23 -0700

    interrupt driven pot read

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
 #include <util/delay.h>
 
 
@@ -6,7 +8,17 @@
 #define LED_OFF() PORTB |= _BV(5)
 
 #include "lcd.h"
-#include <avr/interrupt.h>
+
+static uint8_t g_cur_pot=0;
+#define g_n_pots 2
+static uint8_t g_pots[g_n_pots];
+
+static uint8_t g_dirty=1;
+
+void read_pots()
+{
+    ADCSRA |= (1<<ADSC);
+}
 
 int main(void) 
 {
@@ -16,16 +28,12 @@ int main(void)
 
     //setup ADC 
     // prescaler: 128
-    ADCSRA |= (1<<ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);// | (1<<ADFR);
+    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0)| (1<<ADEN);
     ADMUX = (1<<REFS0)|(1<<ADLAR);
 
-
-    //turn on adc sampling
-    ADCSRA |= (1<<ADSC) | (1<<ADATE);
-
-    //free running mode
-    ADCSRB = 0;
-
+    //trigger one conversion for interrupt
+    ADCSRA |= (1<<ADIE)|(1<<ADSC);
+    sei();
 
 
     lcd_init();
@@ -35,14 +43,51 @@ int main(void)
 
     lcd_cursor(0,0);
     lcd_write("A0:");
-    static char buf[10];
+    lcd_cursor(0,1);
+    lcd_write("A1:");
 
     for (;;) {
-        sprintf(buf, "%02d", ADCH);
-        lcd_cursor(3,0);
-        lcd_write(buf);
+        if (g_dirty) {
+            paint();
+            g_dirty = 0;
+        }
+        sleep_enable();
+        sleep_cpu();
+        sleep_disable();
     }
 
     
     return 0;
+}
+
+void paint()
+{
+    static char buf[10];
+    sprintf(buf, "%03d", g_pots[0]);
+    lcd_cursor(3,0);
+    lcd_write(buf);
+
+    sprintf(buf, "%03d", g_pots[1]);
+    lcd_cursor(3,1);
+    lcd_write(buf);
+
+}
+
+ISR(ADC_vect)
+{
+
+    g_pots[g_cur_pot] = ADCH;
+    g_dirty = 1;
+
+
+    int new_pot = g_cur_pot+1;
+    if (new_pot >= g_n_pots) {
+        new_pot = 0;
+    }
+    g_cur_pot = new_pot;
+
+    // switch input pin
+    ADMUX = (ADMUX&0xF0) | (new_pot & 0x0F);
+    // trigger another interrupt
+    ADCSRA |= (1<<ADSC);
 }
